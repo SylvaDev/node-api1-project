86deb4f13a23ddd25bbd65498111cce37bf2018d
SylvaDev <irvingcsylva@gmail.com>
2025-06-10 02:19:22 -0700

    added endpoint for posting new users and error handling logic. all post tests passing

--- api/server.js
+++ api/server.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const User = require('./users/model');
-const server = express();
+const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({ message: 'Please provide name and bio for the user' })
+    } else {
+        User.insert(user)
+            .then(user => {
+                res.status(201).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({ 
+                    message: 'There was an error while saving the user to the database',
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -33,6 +53,7 @@ server.get('/api/users/:id', (req, res) => {
             })
         })
 })
+
 
 
 
