1eef61f0d0c9101802c3f87b9f765745b9ac0b30
SylvaDev <irvingcsylva@gmail.com>
2025-06-10 01:17:41 -0700

    added start server logic

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+server.listen(port, () => {
+    console.log(`Server is starting...`)
+    console.log(`Server is listen on http://localhost:${port}`)
+});
